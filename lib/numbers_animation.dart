718feae0d76d191496017bf804caec80e251c49b
Zoey Child <imperialoctopus@gmail.com>
2021-02-07 18:48:00 +0000

    Update numbers animation

--- lib/numbers_animation.dart
+++ lib/numbers_animation.dart
@@ -1,44 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:supernova_flutter_ui_toolkit/keyframes.dart';
 
-// Function creating the exit end enter aniamtions for the [ClockCounter] widgets
-//
-// [controller] is the controller that plays the animations,
-// [keyframes] is the breakpoints for the animation,
-// [interval] is the [Interval] used to delay sequential number animations
+/// Function creating the exit end enter aniamtions for the [ClockCounter] widgets
+///
+/// [controller] is the controller that plays the animations,
+/// [keyframes] is the breakpoints for the animation,
+/// [interval] is the [Interval] used to delay sequential number animations
 Animation<double> setupTranslationY(AnimationController controller,
     List<Keyframe<double>> keyframes, Interval interval) {
   return Interpolation(keyframes: keyframes)
       .animate(CurvedAnimation(parent: controller, curve: interval));
 }
 
-// Widget building the [AnimatedBuilder] for the clock face numbers
+/// Widget building the [AnimatedBuilder] for the clock face numbers.
 class NumbersAnimation extends StatelessWidget {
+  /// Widget building the [AnimatedBuilder] for the clock face numbers.
   NumbersAnimation({
-    Key key,
+    Key? key,
     this.child,
-    this.animationController,
-    @required this.keyframes,
-    @required Interval interval,
-  })  : assert(keyframes != null && keyframes.length > 0),
-        assert(interval != null),
-        this.translationY =
+    required this.animationController,
+    required this.keyframes,
+    required Interval interval,
+  })   : translationY =
             setupTranslationY(animationController, keyframes, interval),
         super(key: key);
 
+  /// Animation for y translation.
   final Animation<double> translationY;
+
+  /// Animation controller for the number animation.
   final AnimationController animationController;
-  final Widget child;
+
+  /// Child of this animation.
+  final Widget? child;
+
+  /// List of keyframes.
   final List<Keyframe<double>> keyframes;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: Listenable.merge([this.translationY]),
-      child: this.child,
+      animation: Listenable.merge([translationY]),
+      child: child,
       builder: (context, widget) {
         return Transform.translate(
-          offset: Offset(0, this.translationY.value),
+          offset: Offset(0, translationY.value),
           child: widget,
         );
       },
