617ec66419df3ceb21f238b31c30211bb846e606
Zoey Child <imperialoctopus@gmail.com>
2021-02-07 18:19:08 +0000

    Update branches layout

--- lib/branches_layout.dart
+++ lib/branches_layout.dart
@@ -1,21 +1,23 @@
-import 'package:humanbeans_clock/bottom_left_branch.dart';
-import 'package:humanbeans_clock/bottom_right_branch.dart';
-import 'package:humanbeans_clock/clock_ui_inherited_model.dart';
-import 'package:humanbeans_clock/clock_counter.dart';
-import 'package:humanbeans_clock/top_left_branch.dart';
-import 'package:humanbeans_clock/top_right_branch.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
-// Widget holding all the four branches on the screen
-//
-// Each branch is aligned to one of the corners of the parent
-// widget and it goes litle bit off the screen to hide any
-// edges of the iamges when rotating animation plays
-// The bird [FlareActor] is positioned so that simulates
-// position behind the bottom right branch
+import 'bottom_left_branch.dart';
+import 'bottom_right_branch.dart';
+import 'clock_counter.dart';
+import 'clock_ui_inherited_model.dart';
+import 'top_left_branch.dart';
+import 'top_right_branch.dart';
+
+/// Widget holding all the four branches on the screen
+///
+/// Each branch is aligned to one of the corners of the parent
+/// widget and it goes litle bit off the screen to hide any
+/// edges of the iamges when rotating animation plays
+/// The bird [FlareActor] is positioned so that simulates
+/// position behind the bottom right branch
 class BranchesLayout extends StatelessWidget {
-  const BranchesLayout({Key key}) : super(key: key);
+  /// Widget holding all the four branches on the screen
+  const BranchesLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +25,15 @@ class BranchesLayout extends StatelessWidget {
       const Positioned(
         top: 0,
         left: -1,
-        child: const TopLeftBranch(),
+        child: TopLeftBranch(),
       ),
       const Positioned(
         top: 0,
         right: -1,
-        child: const TopRightBranch(),
+        child: TopRightBranch(),
       ),
-      Center(child: ClockCounter()),
-      const Positioned(bottom: 0, left: -1, child: const BottomLeftBranch()),
+      const Center(child: ClockCounter()),
+      const Positioned(bottom: 0, left: -1, child: BottomLeftBranch()),
       // The [FlareActor] subscirbes to the 'birdControls' aspect of
       // the [ClockUiInheritedModel] to use it's value. Shoudn't get notified
       // to rebuild as the model don't check for that aspect
@@ -45,7 +47,7 @@ class BranchesLayout extends StatelessWidget {
       const Positioned(
         bottom: 0,
         right: -1,
-        child: const BottomRightBranch(),
+        child: BottomRightBranch(),
       )
     ]);
   }
